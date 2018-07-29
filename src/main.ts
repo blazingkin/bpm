cd97ace4a4f0f16008835a2e9a188eaf377aa500
James Vaughan <dev.jamesvaughan@gmail.com>
2018-07-28 22:05:33 -0300

    Create basic CLI for program

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,28 @@
-import * as yargs from 'yargs'
-import { normalize } from 'path'
+import * as yargs from "yargs"
 
-let pathOptions: yargs.Options = {
-    describe: 'path to pulse directory',
-    default: '.',
-    alias: 'p',
-    normalize: true
-}
+const pathOptions: yargs.Options = {
+    alias: "p",
+    default: ".",
+    describe: "path to pulse directory",
+    normalize: true}
 
 // In the future, use the method here:
 //   https://github.com/yargs/yargs/blob/master/docs/api.md#envprefix
 // to allow users to store cardiovascular credentials in env variables
-let argv: yargs.Arguments = yargs
-                                        //flat?
-    .scriptName('bpm')
-    .command('init [path]', 'initialize a blank pulse', {
-        path: pathOptions
-    })
-    .command(['beat', '$0'], 'synchronize dependencies')
-    .help('h')
+const argv: yargs.Arguments = yargs
+    .scriptName("bpm")
+    .command("init [path]", "initialize a blank pulse", {path: pathOptions})
+    .command(["beat", "$0"], "synchronize dependencies")
+    .help("h")
     .argv
-
-console.log(argv)
 
 switch (argv._[0]) {
     case "init":
-        initialize(argv.path)
+        // TODO: CLI helper (like yeoman or npm init)
+        console.log("not implemented. feel free to help at\n\
+\t https://github.com/J-Vaughan/bpm")
         break
 
     default:
         break
-}
-
-function initialize(path: String) {
-    // make and fill directory, overwrite optional (prompt)
 }
