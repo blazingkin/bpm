57d65c03553ab8499a9930fe4991e868200e036f
James Vaughan <dev.jamesvaughan@gmail.com>
2018-07-28 22:05:45 -0300

    Add scrapped files

---
+++ src/project.ts
@@ -0,0 +1,19 @@
+// Publishing packages is an important part of package management, but it comes
+// after everything else, not before.
+/*
+export class Project extends Dependency {
+    // TODO: Make spdx license thing
+    public constructor(name: string,
+                       version: string,
+                       public author: string,
+                       public dependencies: [Dependency],
+                       public synopsis: string =
+                        "Short description of your project",
+                       public description: string =
+                        "Longer description of your project",
+                       public license: string = "MIT",
+                       public repository: string = "") {
+        super(name, version)
+    }
+}
+*/
--- src/pulse.ts
+++ src/pulse.ts
@@ -1,3 +1,22 @@
-export function initialize() {
-    return 0
+// Forcing style guidelines is not a priority, nor is it a main feature or any
+// modern package manager. I'm leaving this for later.
+/*
+export function initialize(project: Project, path: string) {
+    const prevDir: string = process.cwd()
+
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path)
+    }
+
+    process.chdir(path)
+
+    fs.open("Heartbeat", "w", (err, fd) => {
+        if (err) { throw err }
+        fs.write(fd, JSON.stringify(project, undefined, 4), (werr, writ, str) => {
+            if (werr) {throw werr}
+        })
+    })
+
+    process.chdir(prevDir)
 }
+*/
